f67535fdf6f22f0340ce4f6712681e2282aa72fa
taishikato <taishi.k0903@gmail.com>
2023-07-29 15:06:32 -0700

    Add sonner library for toast notifications

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import { sleep } from "@/utils/sleep";
 import axios, { AxiosError } from "axios";
 import { Loader2 } from "lucide-react";
 import { useState } from "react";
+import { Toaster, toast } from "sonner";
 
 export default function Home() {
   const [sendingFile, setSendingFile] = useState(false);
@@ -18,7 +19,9 @@ export default function Home() {
       if (files && files[0]) {
         await Submit(files[0]);
       }
+      toast.success("Event has been created");
     } catch (err) {
+      toast.error(JSON.stringify(err));
     } finally {
       setSendingFile(false);
     }
@@ -37,40 +40,43 @@ export default function Home() {
   };
 
   return (
-    <main className="flex flex-col items-center justify-start min-h-screen p-24 gap-y-20">
-      <div className="z-10 items-center justify-between w-full max-w-5xl font-mono text-sm lg:flex">
-        <p
-          className={`${syne.className} font-bold fixed top-0 left-0 flex justify-center w-full pt-8 pb-6 border-b border-gray-300 bg-gradient-to-b from-zinc-200 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30`}
-        >
-          Book2Audio
-        </p>
-      </div>
-      <div className="relative flex items-center justify-center w-full h-full p-20 overflow-hidden border border-gray-800 rounded-lg bg-background">
-        <Meteors />
-        <div className="flex mt-4 text-sm leading-6 text-gray-600">
-          {sendingFile ? (
-            <Loader2 className="w-8 h-8 animate-spin text-gray-50" />
-          ) : (
-            <label
-              htmlFor="file-upload"
-              className="relative text-3xl font-semibold rounded-md cursor-pointer text-gray-50"
-            >
-              <span>Upload a PDF file</span>
-              <input
-                disabled={sendingFile}
-                accept="application/pdf"
-                onChange={onChangeFile}
-                id="file-upload"
-                name="file-upload"
-                type="file"
-                className="sr-only"
-              />
-            </label>
-          )}
-          {/* <p className="pl-1">or drag and drop</p> */}
+    <>
+      <Toaster richColors />
+      <main className="flex flex-col items-center justify-start min-h-screen p-24 gap-y-20">
+        <div className="z-10 items-center justify-between w-full max-w-5xl font-mono text-sm lg:flex">
+          <p
+            className={`${syne.className} font-bold fixed top-0 left-0 flex justify-center w-full pt-8 pb-6 border-b border-gray-300 bg-gradient-to-b from-zinc-200 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30`}
+          >
+            Book2Audio
+          </p>
         </div>
-        <div className="pointer-events-none absolute inset-0 h-full bg-[radial-gradient(circle_at_50%_120%,rgba(120,119,198,0.3),rgba(255,255,255,0))]" />
-      </div>
-    </main>
+        <div className="relative flex items-center justify-center w-full h-full p-20 overflow-hidden border border-gray-800 rounded-lg bg-background">
+          <Meteors />
+          <div className="flex mt-4 text-sm leading-6 text-gray-600">
+            {sendingFile ? (
+              <Loader2 className="w-8 h-8 animate-spin text-gray-50" />
+            ) : (
+              <label
+                htmlFor="file-upload"
+                className="relative text-3xl font-semibold rounded-md cursor-pointer text-gray-50"
+              >
+                <span>Upload a PDF file</span>
+                <input
+                  disabled={sendingFile}
+                  accept="application/pdf"
+                  onChange={onChangeFile}
+                  id="file-upload"
+                  name="file-upload"
+                  type="file"
+                  className="sr-only"
+                />
+              </label>
+            )}
+            {/* <p className="pl-1">or drag and drop</p> */}
+          </div>
+          <div className="pointer-events-none absolute inset-0 h-full bg-[radial-gradient(circle_at_50%_120%,rgba(120,119,198,0.3),rgba(255,255,255,0))]" />
+        </div>
+      </main>
+    </>
   );
 }
